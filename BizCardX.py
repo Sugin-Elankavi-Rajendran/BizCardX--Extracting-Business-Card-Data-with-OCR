ea44232b35a8e377c8e68f286c3d39dea2046719
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-08-29 17:00:43 +0530

    added file uploader widget

--- BizCardX.py
+++ BizCardX.py
@@ -4,3 +4,17 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.header("Business Card Details Extraction:")
+label = "Upload the Business Card"
+st.file_uploader(
+    label,
+    type= ['png', 'jpg'],
+    accept_multiple_files=False, 
+    key=None, 
+    help=None, 
+    on_change=None, 
+    args=None, 
+    kwargs=None, 
+    *, 
+    disabled=False, 
+    label_visibility="visible"
+)
