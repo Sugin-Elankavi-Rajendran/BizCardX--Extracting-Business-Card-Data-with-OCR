a7d22a575eebfec08ffa79418de6ad75e5211b95
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-09-19 21:19:27 +0530

    created mysql table

--- BizCardX.py
+++ BizCardX.py
@@ -2,6 +2,7 @@ import streamlit as st
 import easyocr
 from PIL import Image
 import numpy as np
+import mysql.connector
 
 ###############
 
@@ -35,3 +36,35 @@ if uploaded_file is not None:
         st.subheader("Extracted Text")
         st.write(extracted_text)
         st.write(extracted_lines)
+
+#######################
+
+connection = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "12345",
+    database = "cards"
+)
+cursor = connection.cursor()
+
+create_table_for_company_info = """
+CREATE TABLE company_info (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    phone1 VARCHAR(15),
+    phone2 VARCHAR(15),
+    website VARCHAR(255),
+    email VARCHAR(255),
+    address VARCHAR(255),
+    city VARCHAR(255),
+    state VARCHAR(255),
+    postal_code VARCHAR(10),
+    additional_info TEXT
+)"""
+
+cursor.execute(create_table_for_company_info)
+
+connection.commit()
+connection.close()
+
+#################################
