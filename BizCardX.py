6f73935edb8ea8485e85e00b10c96801364661ea
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-09-20 11:00:09 +0530

    Preprocessed the image for extraction

--- BizCardX.py
+++ BizCardX.py
@@ -3,6 +3,9 @@ import easyocr
 from PIL import Image
 import numpy as np
 import mysql.connector
+import cv2
+import tempfile
+
 
 ###############
 
@@ -21,21 +24,37 @@ uploaded_file = st.file_uploader(
 
 ###############
 
+if uploaded_file is not None:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_file:
+        temp_file.write(uploaded_file.read())
+        temp_file_path = temp_file.name
+    image = cv2.imread(temp_file_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, binary_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY)
+    denoised_image = cv2.fastNlMeansDenoising(binary_image, None, h=10, templateWindowSize=7, searchWindowSize=21)
+ 
+###############
+
+if uploaded_file and denoised_image is not None:
+    cols = st.columns(2)
+    with cols[0]:
+        st.image(uploaded_file, use_column_width=True, caption="Uploaded Image")
+    with cols[1]:
+        st.image(denoised_image, use_column_width=True, caption="Processed Image")
+
+###############
+
 extracted_lines = []
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-
-    if st.button("Extract Text"):
-        np_image = np.array(image)
-        result = reader.readtext(np_image)
-        for text in result:
-            extracted_lines.append(text[1])
-        extracted_text = "\n".join(extracted_lines)
-        st.subheader("Extracted Text")
-        st.write(extracted_text)
-        st.write(extracted_lines)
+if st.button("Extract Text"):
+    np_image = np.array(denoised_image)
+    result = reader.readtext(np_image)
+    for text in result:
+        extracted_lines.append(text[1])
+    extracted_text = "\n".join(extracted_lines)
+    st.subheader("Extracted Text")
+    st.write(extracted_text)
+    st.write(extracted_lines)
 
 #######################
 
@@ -47,24 +66,31 @@ connection = mysql.connector.connect(
 )
 cursor = connection.cursor()
 
-create_table_for_company_info = """
-CREATE TABLE company_info (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    phone1 VARCHAR(15),
-    phone2 VARCHAR(15),
-    website VARCHAR(255),
-    email VARCHAR(255),
-    address VARCHAR(255),
-    city VARCHAR(255),
-    state VARCHAR(255),
-    postal_code VARCHAR(10),
-    additional_info TEXT
-)"""
-
-cursor.execute(create_table_for_company_info)
+table_exists = False
+cursor.execute("SHOW TABLES LIKE 'company_info'")
+if cursor.fetchone():
+    table_exists = True
+    
+if not table_exists:
+    create_table_for_company_info = """
+    CREATE TABLE company_info (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        phone1 VARCHAR(15),
+        phone2 VARCHAR(15),
+        website VARCHAR(255),
+        email VARCHAR(255),
+        address VARCHAR(255),
+        city VARCHAR(255),
+        state VARCHAR(255),
+        postal_code VARCHAR(10),
+        additional_info TEXT
+    )"""
+    
+    cursor.execute(create_table_for_company_info)
 
 connection.commit()
 connection.close()
 
 #################################
+
