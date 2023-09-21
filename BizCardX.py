53ba1a2c5f0148a6a594959a5a57b24f27b05fba
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-09-21 13:34:43 +0530

    extracted city name

--- BizCardX.py
+++ BizCardX.py
@@ -56,6 +56,8 @@ if st.button("Extract Text"):
     st.subheader("Extracted Text")
     st.write(extracted_text)
     st.write(extracted_lines)
+
+#
     
     website_pattern = r'www[\w\-]+\.com'
     website = re.search(website_pattern, extracted_text)
@@ -63,13 +65,17 @@ if st.button("Extract Text"):
     if website:
         website_name = website.group()
         st.write("Website:", website_name)
-    
+   
+#
+ 
     phone_pattern = r'\+\d{3}-\d{3}-\d{4}'
     phone_numbers = re.findall(phone_pattern, extracted_text)
     
     if phone_numbers:
         for phone_number in phone_numbers:
             st.write("Phone Number:", phone_number)
+
+#
     
     email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
     email = re.search(email_pattern, extracted_text)
@@ -77,6 +83,8 @@ if st.button("Extract Text"):
     if email:
         email_id = email.group()
         st.write("Email Address:", email_id)
+
+#
     
     address_pattern = r'\d+\s+ABC St'
     address_name = re.search(address_pattern, extracted_text)
@@ -84,7 +92,16 @@ if st.button("Extract Text"):
     if address_name:
         addresses = address_name.group()
         st.write("Address:", addresses)
-        
+
+#
+    
+    city_pattern = r',\s*([A-Za-z\s]+)'
+    city_name = re.search(city_pattern, extracted_text)
+    
+    if city_name:
+        cities = city_name.group(1)
+        st.write("City:", cities)
+          
 #######################
 
 connection = mysql.connector.connect(
