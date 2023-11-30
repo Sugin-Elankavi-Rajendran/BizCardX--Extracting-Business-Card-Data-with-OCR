58dcd906b3b99cecfa480f0b4c62e1d3dc651207
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-11-30 14:11:01 +0530

    made the dash wide and added extract button

--- BizCardX.py
+++ BizCardX.py
@@ -7,21 +7,26 @@ import mysql.connector
 
 ##########################
 
+st.set_page_config(layout="wide")
 st.title ("BizCardX Data Extraction")
 image = st.file_uploader("Choose a business card image", type=["jpg", "png", "jpeg"])
 
-if image:
-    st.image(image, caption="Uploaded Business Card", use_column_width=True)
-    reader = easyocr.Reader(["en"])
-    image_bytes = io.BytesIO(image.read())
-    image_pil = Image.open(image_bytes)
-    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
-    image_pil.save(temp_file.name)
-    results = reader.readtext(temp_file.name)
-    temp_file.close()
+if st.button("Extract"):
+    if image:
+        st.image(image, caption="Uploaded Business Card", use_column_width=True)
+        reader = easyocr.Reader(["en"])
+        image_bytes = io.BytesIO(image.read())
+        image_pil = Image.open(image_bytes)
+        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
+        image_pil.save(temp_file.name)
+        results = reader.readtext(temp_file.name)
+        temp_file.close()
 
-    for result in results:
-        st.write(result[1])
+        for result in results:
+            st.write(result[1])
+
+    else:
+        st.write("PLEASE UPLOAD TO EXTRACT DATA")
 
 ################
 
