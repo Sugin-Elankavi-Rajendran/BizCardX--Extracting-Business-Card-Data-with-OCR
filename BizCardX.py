b5267514a8037bc1ce1d4947536cac1dbe60e1ad
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-11-30 10:58:41 +0530

    extracted image from results

--- BizCardX.py
+++ BizCardX.py
@@ -1,5 +1,23 @@
 import streamlit as st
+import easyocr
+from PIL import Image
+import io
+import tempfile
 
 ##########################
 
 st.title ("BizCardX Data Extraction")
+image = st.file_uploader("Choose a business card image", type=["jpg", "png", "jpeg"])
+
+if image:
+    st.image(image, caption="Uploaded Business Card", use_column_width=True)
+    reader = easyocr.Reader(["en"])
+    image_bytes = io.BytesIO(image.read())
+    image_pil = Image.open(image_bytes)
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
+    image_pil.save(temp_file.name)
+    results = reader.readtext(temp_file.name)
+    temp_file.close()
+
+    for result in results:
+        st.write(result[1])
