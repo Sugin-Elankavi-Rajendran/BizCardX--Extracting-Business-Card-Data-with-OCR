69b0cb105c0b8727417b1d1076e17add5e471999
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-11-30 16:34:38 +0530

    extracted the email

--- BizCardX.py
+++ BizCardX.py
@@ -3,8 +3,9 @@ import easyocr
 import cv2
 import mysql.connector
 import numpy as np
+import re
 
-##########################
+#######################################################
 
 st.set_page_config(layout="wide")
 st.title("BizCardX Data Extraction")
@@ -16,38 +17,51 @@ if st.button("Extract"):
         reader = easyocr.Reader(["en"], gpu=False)
         results = reader.readtext(img)
         col1, col2 = st.columns(2)
-        col1.image(img, caption="Original Business Card", use_column_width=True)
+        col1.image(image, caption="Original Business Card", use_column_width=True)
         col2.image(img, caption="Processed Business Card", use_column_width=True)
         
+        answer = []
         for result in results:
-            st.write(result[1])
+            answer.append(result[1])
+        st.write(answer)
 
-        ################
+##########################################################
 
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="12345",
-            database="cards"
-        )
+        email_pattern = r"\b[A-Za-z0-9.+_%-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+        emails = []
+        for result in results:
+            m = result[1]
+            found_emails = re.findall(email_pattern, m)
+            emails.extend(found_emails)
 
-        mycursor = mydb.cursor()
+        st.write("E-Mail:", emails[0])
 
-        mycursor.execute("""
-            CREATE TABLE IF NOT EXISTS card_info (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                company_name VARCHAR(255),
-                card_holder_name VARCHAR(255),
-                designation VARCHAR(255),
-                mobile_number INT,
-                phone_number INT,
-                email_address VARCHAR(255),
-                website_URL VARCHAR(255),
-                area VARCHAR(255),
-                city VARCHAR(255),
-                state VARCHAR(255),
-                pincode INT
-            )
-        """)
+##########################################################
+        
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="12345",
+    database="cards"
+)
 
-        sql = "INSERT INTO card_info (company_name, card_holder_name, designation, mobile_number, phone_number, email_address, website_URL, area, city, state, pincode) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+mycursor = mydb.cursor()
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS card_info (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        company_name VARCHAR(255),
+        card_holder_name VARCHAR(255),
+        designation VARCHAR(255),
+        mobile_number INT,
+        phone_number INT,
+        email_address VARCHAR(255),
+        website_URL VARCHAR(255),
+        area VARCHAR(255),
+        city VARCHAR(255),
+        state VARCHAR(255),
+        pincode INT
+    )
+""")
+
+sql = "INSERT INTO card_info (company_name, card_holder_name, designation, mobile_number, phone_number, email_address, website_URL, area, city, state, pincode) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
